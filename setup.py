9db524b8631ab5b30769753d33c32747f0daf64d
王大平 <262137703@163.com>
2021-01-05 17:51:23 +0800

    版本更新0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fdmutils',
-    version='0.1.0',
+    version='0.1.1',
     author='fdmseven',
     author_email='wrp2003000@163.com',
     packages=["fdmutils"],
